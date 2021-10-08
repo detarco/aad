af767186243ca4a0751e24165e28e38d20cbbbdf
detarco <51322251+detarco@users.noreply.github.com>
2021-10-08 18:42:41 +0200

    Shared references examples

---
+++ app/src/main/java/com/detarco/add_playground/ut02_sharedpreferences/LocalDataSource.kt
@@ -0,0 +1,73 @@
+package com.detarco.add_playground.ut02_sharedpreferences
+
+import android.annotation.SuppressLint
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import androidx.security.crypto.EncryptedSharedPreferences
+import androidx.security.crypto.MasterKey
+import com.detarco.add_playground.R
+
+class LocalDataSource(private val context: AppCompatActivity) {
+
+    private val sharedPref = context.getSharedPreferences(
+        context.getString(R.string.preference_file_key), Context.MODE_PRIVATE
+    )
+
+    val masterKey = MasterKey.Builder(context)
+        .setKeyScheme(MasterKey.KeyScheme.AES256_GCM)
+        .build()
+
+    private val encryptSharedPref = EncryptedSharedPreferences.create(
+        context,
+        context.getString(R.string.encrypted_preference_file_key),
+        masterKey,
+        EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
+        EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
+    )
+
+    //val sharedPref = context.getPreferences(Context.MODE_PRIVATE)
+
+    @SuppressLint("CommitPrefEdits")
+    fun saveAsync(key: String, data: String) {
+        val edit = sharedPref.edit()
+        edit?.putString(key, data)
+        edit?.apply()
+    }
+
+    @SuppressLint("CommitPrefEdits")
+    fun saveSync(key: String, data: String) {
+        val edit = sharedPref.edit()
+        edit?.putString(key, data)
+        edit?.commit()
+    }
+
+    fun shortSaveAsync(key: String, data: String) {
+        with(sharedPref.edit()) {
+            putString(key, data)
+            apply()
+        }
+    }
+
+    fun read(key: String): String? {
+        return sharedPref.getString(key, "valor_por_defecto")
+    }
+
+    @SuppressLint("CommitPrefEdits")
+    fun saveAsyncEncrypt(key: String, data: String) {
+        val edit = encryptSharedPref.edit()
+        edit?.putString(key, data)
+        edit?.apply()
+    }
+
+    @SuppressLint("CommitPrefEdits")
+    fun saveSyncEncrypt(key: String, data: String) {
+        val edit = encryptSharedPref.edit()
+        edit?.putString(key, data)
+        edit?.commit()
+    }
+
+    fun readEncrypt(key: String): String? {
+        return encryptSharedPref.getString(key, "valor_por_defecto")
+    }
+
+}
---
+++ app/src/main/java/com/detarco/add_playground/ut02_sharedpreferences/SharedPreferencesActivity.kt
@@ -0,0 +1,67 @@
+package com.detarco.add_playground.ut02_sharedpreferences
+
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.detarco.add_playground.R
+
+class SharedPreferencesActivity:AppCompatActivity(){
+    private val TAG = SharedPreferencesActivity::class.java.canonicalName
+
+    override fun onCreate(savedInstanceState: Bundle?){
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shared_preferences)
+        initExampleAsync()
+        initExampleEncryptedAsync()
+        initExampleSync()
+        initExampleEncryptedSync()
+    }
+
+    private fun initExampleAsync() {
+        val localDataSource = LocalDataSource(this)
+        localDataSource.saveAsync("AsyncExample", "Primer Ejemplo de Shared Prefs Asíncrono sin Encriptar")
+        val data = localDataSource.read("AsyncExample")
+        //Log.d(TAG, data!!)
+        data?.let{ aSyncEx ->
+            Log.d(TAG, aSyncEx)
+        }
+    }
+
+    private fun initExampleEncryptedAsync() {
+        val localDataSource = LocalDataSource(this)
+        localDataSource.saveAsyncEncrypt("EncryptedAsyncExample", "Ejemplo de Shared Prefs Asíncrono Encriptado")
+        val data = localDataSource.readEncrypt("EncryptedAsyncExample")
+        data?.let{  encryptedASyncEx ->
+            Log.d(TAG, encryptedASyncEx)
+        }
+    }
+
+    private fun initExampleSync(){
+        val localDataSource = LocalDataSource(this)
+        localDataSource.saveSync("SyncExample", "Ejemplo de Shared Prefs Síncrono")
+        val data = localDataSource.read("SyncExample")
+
+        data?.let { syncEx ->
+            Log.d(TAG, syncEx)
+        }
+
+        /**
+         *  data?.run{
+         *      Log.d(TAG, this)
+         *  }
+         */
+
+    }
+
+    private fun initExampleEncryptedSync(){
+        val localDataSource = LocalDataSource(this)
+        localDataSource.saveSyncEncrypt("EncryptedSyncExample","Ejemplo de Shared Prefs Síncrono Encriptado")
+        val data = localDataSource.readEncrypt("EncryptedSyncExample")
+
+        data?.let{  encryptedSyncEx ->
+            Log.d(TAG, encryptedSyncEx)
+
+        }
+    }
+
+}
