fb9c063e7f8bc9efc0df72d509bd9af57662a788
detarco <51322251+detarco@users.noreply.github.com>
2021-11-04 20:40:00 +0100

    Working on Databases

---
+++ app/src/main/java/com/detarco/add_playground/ut03/data/Entities.kt
@@ -0,0 +1,14 @@
+package com.detarco.add_playground.ut03.data
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName="user")
+data class UserEntity (
+    @PrimaryKey val id: Int,
+    //Nombre de tabla
+    @ColumnInfo(name = "name") val name: String,
+    @ColumnInfo(name = "username") val username: String,
+    @ColumnInfo(name = "email") val email: String?
+)
---
+++ app/src/main/java/com/detarco/add_playground/ut03/data/UserDao.kt
@@ -0,0 +1,34 @@
+package com.detarco.add_playground.ut03.data
+
+import androidx.room.*
+
+//Data access object
+@Dao
+interface UserDao {
+
+    @Query("SELECT * FROM user")
+    fun getAll(): List<UserEntity>
+
+    //Todos aquellos usuarios cuyo id sea = 1
+    @Query("SELECT * FROM user WHERE id= :id")
+    fun findById(id: Int): UserEntity?
+
+    //Todos los usuarios que estén en un listado de id
+    @Query("SELECT * FROM user WHERE id IN (:userIds)")
+    fun findAllByIds(userIds: IntArray): List<UserEntity>
+
+    //Que me devuelva todos los usuarios que el nombre sea igual a un
+    //  parámetro y que solo devuelva un único valor
+    @Query("SELECT * FROM user WHERE name LIKE :name AND :username LIMIT 1 ")
+    fun findByName(name:String, username: String): UserEntity?
+
+    @Insert
+    fun insert(userEntity: UserEntity)
+
+    @Delete
+    fun delete(userEntity: UserEntity)
+
+    @Update
+    fun update(userEntity: UserEntity)
+
+}
